537eeb861e8241eb737d0f8a01a5f05352900872
Fredrik von Post <fredrik.v.post@gmail.com>
2025-02-10 11:26:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arch_VaderData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
