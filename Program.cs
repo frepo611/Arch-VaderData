d64e907b4dd63cb647606ea4c8e799bc5f9a3697
Erik Nylund <erik.nylund@campusnykoping.se>
2025-02-10 22:24:59 +0100

    Avg temp och hum

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetData.AverageTempAndHumidity();
+
+            Console.ReadLine();
         }
     }
 }
