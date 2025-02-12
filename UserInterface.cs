055ac79f7fc9fba67c32269c03c8e78e35d9e9bd
Fredrik von Post <fredrik.v.post@gmail.com>
2025-02-12 14:39:56 +0100

    Add User Interface

---
+++ UserInterface.cs
@@ -0,0 +1,31 @@
+﻿namespace Arch_VaderData;
+
+public class UserInterface
+{
+    private Window _mainMenu;
+    public UserInterface()
+    {
+        _mainMenu = new Window("Main menu", 0, 0, GetMenuItems<Menues.Main>());
+        Console.CursorVisible = false;
+    }
+    public void Run()
+    {
+        Console.Clear();
+        _mainMenu.Draw();
+
+    }
+    private static bool TryParseInput<T>(out T input) where T : struct
+    {
+        var rawInput = Console.ReadKey(true).KeyChar.ToString();
+
+        return Enum.TryParse<T>(rawInput, out input);
+    }
+    public List<string> GetMenuItems<TEnum>() where TEnum : Enum
+    {
+        var results = new List<string>();
+        foreach (var menuItem in Enum.GetValues(typeof(TEnum)))
+        {
+            results.Add($"{(int)menuItem}. {menuItem.ToString()?.Replace('_', ' ')}");
+        }
+        return results;
+    }
