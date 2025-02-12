dee9bc14caddc2bcfb1f4cf6389a18a23334f90e
Erik Nylund <erik.nylund@campusnykoping.se>
2025-02-12 16:38:15 +0100

    La till vad som är kvar att göra i meny

--- Menues.cs
+++ Menues.cs
@@ -8,7 +8,7 @@ public class Menues
         Read_data,
         Outdoor_data,
         Indoor_data,
-        Write_file,
+        Write_file, // Att göra
     }
     public enum Outdoor
     {
@@ -16,10 +16,10 @@ public class Menues
         Search_date,
         Show_warmest_to_coldest,
         Show_driest_to_most_humid,
-        Show_mold_risk,
-        Meterological_autumn,
-        Meterological_winter,
-        Show_pleasant_days,
+        Show_mold_risk, // Att göra
+        Meterological_autumn, // Att göra
+        Meterological_winter, // Att göra
+        Show_pleasant_days, // Att göra??
     }
     public enum Indoor
     {
@@ -27,7 +27,7 @@ public class Menues
         Search_date,
         Show_warmest_to_coldest,
         Show_driest_to_most_humid,
-        Show_mold_risk,
-        Show_open_balcony_door_times,
+        Show_mold_risk, // Att göra
+        Show_open_balcony_door_times, // Att göra??
     }
 }
