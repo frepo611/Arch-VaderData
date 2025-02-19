034f70082c1e94d46452d4acdd0848c2a2c85179
Erik NylunD <erik.nylund@campusnykoping.se>
2025-02-19 10:58:02 +0100

    Rennnss

--- Program.cs
+++ Program.cs
@@ -4,29 +4,7 @@ internal class Program
 {
     private static void Main()
     {
-        string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
-        string parentFolder = Directory.GetParent(baseFolder).Parent.Parent.Parent.FullName;
-        string filePath = Path.Combine(parentFolder, "tempdata5-med fel.txt");
-
-        var weatherData = new WeatherData();
-        if (File.Exists(filePath))
-        {
-            var fileStreamer = new FileReader(filePath, weatherData);
-            fileStreamer.ParseFile();
-        }
-        else
-        {
-            Console.WriteLine("File not found.");
-        }
-        weatherData.AverageData();
-        var data = weatherData.Data;
-        foreach (var entry in data)
-        {
-            Console.WriteLine($"Date: {entry.Key}");
-            {
-                Console.WriteLine($"Innetemperatur: {entry.Value.Indoors}");
-            }
-        }
+        
     }
 }
 
